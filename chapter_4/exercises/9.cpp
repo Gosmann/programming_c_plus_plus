8ee1e644c6a5369be390301e2db5d8dbce349527
Gabriel Gosmann <gabriel.gosmann@gmail.com>
2023-09-15 00:44:51 +0200

    chapter 4 exercise 9

---
+++ chapter_4/exercises/9.cpp
@@ -0,0 +1,45 @@
+// chapter 4 exercise 9
+#include "std_lib_facilities.h"
+
+int main(){
+
+    int i;
+    //unsigned long long int current, sum = 0;
+    int current, sum = 0;           // 31 squares for an int 
+    //double current, sum = 0;      // 1024 squares for a double
+
+    int k, m, g;
+
+    for( i = 0 ; i < 64 ; i++ ){
+    //for( i = 0 ; i < 64*32 ; i++ ){
+        
+        if(i != 0)
+            current *= 2;
+        else
+            current = 1;
+
+        sum += current ;
+        
+        if( sum < 1e9 ){
+            g = i + 1;
+        }
+        if( sum < 1e6 ){
+            m = i + 1;
+        }
+        if( sum < 1e3 ){
+            k = i + 1;
+        }
+        
+        
+        
+        cout << i << " " << current << " " << sum << " \n" ;
+    }
+
+    cout << "1000 : " << k << " squares \n";
+    cout << "1000,000 : " << m << " squares \n";
+    cout << "1000,000,000 : " << g << " squares \n";
+
+
+    
+
+}
