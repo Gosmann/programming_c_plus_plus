c8f94064dbbf03fff326681a303d7e34d5fdd478
Gabriel Gosmann <gabriel.gosmann@gmail.com>
2023-09-14 16:12:53 +0200

    chapter 4 exercise 5

---
+++ chapter_4/exercises/6.cpp
@@ -0,0 +1,36 @@
+// chapter 4 exercise 6
+#include "std_lib_facilities.h"
+
+int main(){
+
+    double num1, num2, res;
+    char operation;
+
+    cout << "Enter 2 numbers and an operation (+, -, *, /): " ;
+    cin >> num1 >> num2 >> operation ;
+
+    switch( operation ){
+        case '+':
+            res = num1 + num2 ;
+            cout <<"The sum of " ;
+            break;
+        case '-':
+            res = num1 - num2 ;
+            cout <<"The subtraction of " ;
+            break;
+        case '*':
+            res = num1 * num2 ;
+            cout <<"The multiplication of " ;
+            break;
+        case '/':
+            res = num1 / num2 ;
+            cout <<"The division of " ;
+            break;
+        default:   
+            cout << "Error running operation ";
+    }
+
+    cout << num1 << " and " << num2 << " is " << res << " \n";
+
+    
+}
